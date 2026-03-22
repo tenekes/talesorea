2cfa20472d9a217c13f4d81c2e31965eb482b13a
Tenekes <r000214@gmail.com>
2026-03-22 12:04:45 +0200

    Force add tina generated files

---
+++ tina/__generated__/client.ts
@@ -0,0 +1,5 @@
+import { createClient } from "tinacms/dist/client";
+import { queries } from "./types";
+export const client = createClient({ cacheDir: 'D:/Projects/talesorea/tina/__generated__/.cache/1774173816116', url: 'https://content.tinajs.io/2.2/content/a76ca79d-a2bc-49a4-ba29-c0fc0ebe226c/github/main', token: 'ffafc957631558f92475687dc353594544811606', queries,  });
+export default client;
+  
