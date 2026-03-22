6f26f19abba97dd692fa0f16e972cbb181505c91
Tenekes <r000214@gmail.com>
2026-03-22 14:58:40 +0200

    feat: rename OreaLogo asset to talesorea1.svg

--- src/config.ts
+++ src/config.ts
@@ -14,10 +14,10 @@ export const SITE = {
   editPost: {
     enabled: true,
     text: "Edit page",
-    url: "https://github.com/tenekes/talesorea/edit/main/src/data/blog/",
+    url: "https://github.com/tenekes/talesorea/edit/main/",
   },
   dynamicOgImage: true,
   dir: "ltr", // "rtl" | "auto"
   lang: "en", // html lang code. Set this empty and default will be "en"
-  timezone: "Asia/Bangkok", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+  timezone: "Europe/Athens", // Default global timezone (IANA format) https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 } as const;
