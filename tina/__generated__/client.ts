4306d196117d29e688974d766d72800df864eca5
Tenekes <r000214@gmail.com>
2026-03-22 11:57:07 +0200

    feat: add initial GraphQL schema, queries, and client configuration for TinaCMS

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: 'D:/Projects/talesorea/tina/__generated__/.cache/1774172542705', url: 'http://localhost:4001/graphql', token: 'ffafc957631558f92475687dc353594544811606', queries,  });
-export default client;
-  
