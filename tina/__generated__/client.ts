268c24b11fd4a3a9349c047d23a3298f4e08dbb8
Tenekes <r000214@gmail.com>
2026-03-22 16:04:01 +0200

    chore: completely remove TinaCMS

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ cacheDir: 'D:/Projects/talesorea/tina/__generated__/.cache/1774187478174', url: 'https://content.tinajs.io/2.2/content/a76ca79d-a2bc-49a4-ba29-c0fc0ebe226c/github/main', token: 'ffafc957631558f92475687dc353594544811606', queries,  });
-export default client;
-  
